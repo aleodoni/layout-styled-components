4bca58d03d89d4eee66a381c7bd5d48adf737113
aleodoni <aleodoni@gmail.com>
2019-08-12 15:02:05 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            <h1>Layout</h1>
+        </Container>
+    )
+}
